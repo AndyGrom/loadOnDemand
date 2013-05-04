669f0e82e1b222afe8a9b2c04c7f734cea0450c3
Andrey Gromozdov <andy.grom@yandex.ru>
2013-05-04 20:07:44 +0400

    dependencies load fix

--- loadOnDemand.js
+++ loadOnDemand.js
@@ -1,6 +1,8 @@
 ﻿/*global angular*/
 (function () {
     'use strict';
+    var regModules = ["ng"];
+    
     var module = angular.module('loadOnDemand', []);
 
     module.factory('scriptCache', ['$cacheFactory', function ($cacheFactory) {
@@ -12,6 +14,7 @@
     module.provider('$loadOnDemand',
         ['$controllerProvider', '$provide', '$compileProvider', '$filterProvider',
             function ($controllerProvider, $provide, $compileProvider, $filterProvider) {
+                
                 var modules = { },
                     providers = {
                         $controllerProvider: $controllerProvider,
@@ -19,63 +22,43 @@
                         $filterProvider: $filterProvider,
                         $provide: $provide // other things
                     };
-                this.$get = ['scriptCache', '$timeout', '$log',
-                    function (scriptCache, $timeout, $log) {
+                this.$get = ['scriptCache', '$timeout', '$log', '$document',
+                    function (scriptCache, $timeout, $log, $document) {
                         return {
                             getConfig: function (name) {
                                 if (!modules[name]) {
-                                    var errorText = 'Module "' + name + '" not configured';
-                                    $log.error(errorText);
+                                    return null;
                                 }
                                 return modules[name];
                             },
                             load: function (name, callback) {
                                 var self = this,
                                     config = self.getConfig(name),
-                                    needRegister = [],
-                                    resourceId = 'script:' + config.script;
-
-                                function register(registerModules) {
-                                    var i, ii, k, invokeQueue, moduleName, moduleFn, invokeArgs, provider;
-                                    if (registerModules) {
-                                        for (k = 0; k < registerModules.length; k++) {
-                                            moduleName = registerModules[k];
-                                            moduleFn = angular.module(moduleName);
-                                            try {
-                                                for (invokeQueue = moduleFn._invokeQueue, i = 0, ii = invokeQueue.length; i < ii; i++) {
-                                                    invokeArgs = invokeQueue[i];
-
-                                                    if (providers.hasOwnProperty(invokeArgs[0])) {
-                                                        provider = providers[invokeArgs[0]];
-                                                    } else {
-                                                        return $log.error("unsupported provider " + invokeArgs[0]);
-                                                    }
-                                                    provider[invokeArgs[1]].apply(provider, invokeArgs[2]);
-                                                }
-                                            } catch (e) {
-                                                if (e.message) {
-                                                    e.message += ' from ' + moduleName;
-                                                }
-                                                $log.error(e.message);
-                                                throw e;
-                                            }
-                                        }
+                                    resourceId = 'script:' + config.script,
+                                    modules = [];
+                                modules.push = function(value) {
+                                    if (this.indexOf(value) == -1) {
+                                        Array.prototype.push.apply(this, arguments);
                                     }
-                                    return null;
+                                };
+                                if (!config) {
+                                    var errorText = 'Module "' + name + '" not configured';
+                                    $log.error(errorText);
+                                    throw errorText;
                                 }
 
                                 function loadScript(url, onLoadScript) {
                                     var scriptId = 'script:' + url,
                                         scriptElement;
                                     if (!scriptCache.get(scriptId)) {
-                                        scriptElement = window.document.createElement('script');
+                                        scriptElement = $document[0].createElement('script');
                                         scriptElement.src = url;
                                         scriptElement.onload = onLoadScript;
                                         scriptElement.onerror = function () {
                                             $log.error('Error loading "' + url + '"');
                                             scriptCache.remove(scriptId);
                                         };
-                                        document.documentElement.children[0].appendChild(scriptElement);
+                                        $document[0].documentElement.appendChild(scriptElement);
                                         scriptCache.put(scriptId, 1);
                                     } else {
                                         $timeout(onLoadScript);
@@ -83,45 +66,53 @@
                                 }
 
                                 function loadDependencies(moduleName, allDependencyLoad) {
+                                    if (regModules.indexOf(moduleName) > -1) {
+                                        return allDependencyLoad();
+                                    }
                                     var loadedModule = angular.module(moduleName),
-                                        requires = [],
-                                        loadedCallback = function (moduleLoaded) {
-                                            if (moduleLoaded) {
-                                                needRegister.push(moduleLoaded);
+                                        requires = getRequires(loadedModule);
+                                    function onModuleLoad(moduleLoaded) {
+                                        if (moduleLoaded) {
 
-                                                var index = requires.indexOf(moduleLoaded);
-                                                if (index > -1) {
-                                                    requires.splice(index, 1);
-                                                }
+                                            var index = requires.indexOf(moduleLoaded);
+                                            if (index > -1) {
+                                                requires.splice(index, 1);
                                             }
-                                            if (requires.length === 0) {
-                                                allDependencyLoad();
-                                            }
-                                        };
-                                    angular.forEach(loadedModule.requires, function (requireModule) {
-                                        try {
-                                            angular.module(requireModule);
-                                        } catch (e) {
-                                            if (/No module/.test(e)) {
-                                                requires.push(requireModule);
+                                        }
+                                        if (requires.length === 0) {
+                                            allDependencyLoad();
+                                        }
+                                    }
+
+                                    angular.forEach(getRequires(loadedModule), function (requireModule) {
+                                        modules.push(requireModule);
+                                        var requireModuleConfig = self.getConfig(requireModule);
+                                        if (requireModuleConfig) {
+                                            loadScript(requireModuleConfig.script, function() {
+                                                loadDependencies(requireModule, function () {
+                                                    onModuleLoad(requireModule);
+                                                });
+                                            });
+                                        } else {
+                                            if (moduleExists(requireModule)) {
+                                                loadDependencies(requireModule, function() {
+                                                    onModuleLoad(requireModule);
+                                                });
+                                            } else {
+                                                $log.warn('module "' + requireModule +"' not loaded and not configured");
+                                                onModuleLoad(regModules);
                                             }
                                         }
                                     });
-                                    angular.forEach(requires, function (requireModule) {
-                                        var requireModuleConfig = self.getConfig(requireModule);
-                                        loadScript(requireModuleConfig.script, function () {
-                                            loadedCallback(requireModule);
-                                        });
-                                    });
 
-                                    loadedCallback();
+                                    onModuleLoad();
                                 }
 
                                 if (!scriptCache.get(resourceId)) {
                                     loadScript(config.script, function () {
-                                        needRegister.push(name);
+                                        modules.push(name);
                                         loadDependencies(name, function () {
-                                            register(needRegister);
+                                            register(providers, modules);
                                             $timeout(function () {
                                                 callback(false);
                                             });
@@ -136,13 +127,18 @@
                             }
                         };
                     }];
-                this.config = function (config) {
+                this.config = function (config, registeredModules) {
                     if (angular.isArray(config)) {
                         angular.forEach(config, function (moduleConfig) {
                             modules[moduleConfig.name] = moduleConfig;
                         });
                     } else {
                         modules[config.name] = config;
+                    }
+                    if (registeredModules) {
+                        angular.forEach(registeredModules, function(name) {
+                            regModules.push(name);
+                        });
                     }
                 };
             }]);
@@ -210,6 +206,56 @@
                 }
             };
         }]);
+    
+    function getRequires(module) {
+        var requires = [];
+        angular.forEach(module.requires, function (requireModule) {
+            if (regModules.indexOf(requireModule) == -1) {
+                requires.push(requireModule);
+            }
+        });
+        return requires;
+    }
+    function moduleExists(moduleName) {
+        try {
+            angular.module(moduleName);
+        } catch (e) {
+            if (/No module/.test(e)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    function register(providers, registerModules) {
+        var i, ii, k, invokeQueue, moduleName, moduleFn, invokeArgs, provider;
+        if (registerModules) {
+            for (k = 0; k < registerModules.length; k++) {
+                moduleName = registerModules[k];
+                moduleFn = angular.module(moduleName);
+                regModules.push(moduleName);
+                try {
+                    for (invokeQueue = moduleFn._invokeQueue, i = 0, ii = invokeQueue.length; i < ii; i++) {
+                        invokeArgs = invokeQueue[i];
+
+                        if (providers.hasOwnProperty(invokeArgs[0])) {
+                            provider = providers[invokeArgs[0]];
+                        } else {
+                            return $log.error("unsupported provider " + invokeArgs[0]);
+                        }
+                        provider[invokeArgs[1]].apply(provider, invokeArgs[2]);
+                    }
+                } catch (e) {
+                    if (e.message) {
+                        e.message += ' from ' + moduleName;
+                    }
+                    $log.error(e.message);
+                    throw e;
+                }
+            }
+        }
+        return null;
+    }
+
 })();
 
 
